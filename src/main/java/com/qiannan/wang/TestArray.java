1cee8317904f1a14990ea64d5474d1ab7623cfac
wangnanhui <wangnanhui@tal.com>
2021-03-18 18:14:06 +0800

    test

---
+++ src/main/java/com/qiannan/wang/TestArray.java
@@ -0,0 +1,17 @@
+package com.qiannan.wang;
+
+public class TestArray {
+	
+	
+	public static void main(String[] args) {
+		Object [] o = new Object[Byte.MAX_VALUE];
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
